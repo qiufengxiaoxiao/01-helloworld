d1a60f911e1672219c5ae23edf0017c8155f638b
sunchongsheng <sunchongsheng888@126.com>
2015-07-20 16:44:25 +0800

    hell0
    world

---
+++ 01-helloworld/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  01-helloworld
+//
+//  Created by qianfeng on 15-5-30.
+//  Copyright (c) 2015年 sun. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!") //swift不需要加@,无需main函数
+println("Hello, World!1")
+println("Hello, World!2")
